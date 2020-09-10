87f3f697f70ce0665f3caa5510d5b0b3921b0ee5
wangweitao <wang.weitao1@byd.com>
2020-09-10 14:42:33 +0800

    优雅重启

--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/fvbock/endless"
-	"log"
-	"syscall"
-
-	"github.com/utf6/go-blog/routers"
-
 	"github.com/utf6/go-blog/pkg/setting"
+	"github.com/utf6/go-blog/routers"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	endless.DefaultReadTimeOut = setting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
-	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	router := routers.InitRouter()
 
-	server := endless.NewServer(endPoint, routers.InitRouter())
-	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("Server err：%v", err)
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<- quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
 	}
+
+	log.Println("Server exiting")
 }
